099ff04003bf199500d48a3b8b460e227752a26b
Alex Lefkowitz <alefkowitz@wustl.edu>
2025-09-03 15:29:31 -0500

    hi

---
+++ src/studio1/Average.java
@@ -0,0 +1,13 @@
+package studio1;
+import java.util.Scanner;
+public class Average {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Yo, sup?");
+        int n1 = in.nextInt();
+        System.out.println("The first of two uintegers to be averaged?");
+        int n2 = in.nextInt();
+        System.out.println("Average of "+n1+" and "+n2+ " is " + ((double)(n1+n2))/2);
+    }
+
+}
